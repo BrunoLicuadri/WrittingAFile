dd0220b323337e76e1c4d09f52f5f31088b660fa
Bruno Licuadri <blicuadri@gmail.com>
2022-04-05 16:28:56 -0300

    changes made to add new lines to an existing file

--- src/application/Prorgam.java
+++ src/application/Prorgam.java
@@ -8,10 +8,10 @@ public class Prorgam {
 
 	public static void main(String[] args) {
 		// Learning how to write a file using BufferedWriter and FileWriter
-		String [] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+		String [] lines = new String[] {"Baltimore", "New York City", "Las Vegas"};
 		String path = "C:\\Users\\Bruno\\Desktop\\Out.txt";
 		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
 			for (String line : lines) {
 				bw.write(line);
 				bw.newLine();
